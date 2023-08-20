0c9886c20df22d4ea929cf7aa639c35965a92794
Titus Denis <deetitos@gmail.com>
2023-08-20 12:43:51 +0100

    commmited the first trial

---
+++ README.md
@@ -0,0 +1,1 @@
+Repository for our printf project
---
+++ _printf.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ *_printf - produces output according to a format
+ *@format: The specified format
+ *
+ *Return: The number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i;
+	char c;
+	char *s;
+	int char_count = 0;
+
+	va_start(args, format);
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '\0')
+				break;
+			if (*format == 's')
+			{
+				s = va_arg(args, char*);
+				write(1, s, strlen(s));
+				char_count += strlen(s);
+			}
+			else if (*format == 'c')
+			{
+				c = va_arg(args, int);
+				write(1, &c, 1);
+				char_count++;
+			}
+			else if (*format == '%')
+			{
+				write(1, format, 1);
+				char_count++;
+			}
+		}
+		else
+		{
+			write(1, format, 1);
+			char_count++;
+		}
+		format++;
+	}
+	va_end(args);
+	return (char_count);
+}
