530dca4b63655d07911f17d35a2f668dad47bf7d
Titus Denis <deetitos@gmail.com>
2023-08-21 01:53:17 +0100

    commited

--- _printf.c
+++ _printf.c
@@ -1,39 +1,5 @@
 #include "main.h"
 /**
- *print_string - prints strings and return the count
- *@str: pointer to the string
- *
- *Return: pointer to the string
- */
-void print_string(char *str)
-{
-	int len = 0;
-
-	while (str[len] != '\0')
-	{
-		len++;
-	}
-	write(1, str, len);
-}
-/**
- *count_string - counts the number of character in a string
- *@s: pointer to the string
- *
- *Return: counts
- */
-int count_string(char *s)
-{
-	int count = 0;
-
-	while (s[count] != '\0')
-	{
-		count++;
-		s++;
-	}
-	return (count);
-}
-
-/**
  *_printf - produces output according to a format
  *@format: The specified format
  *
@@ -43,27 +9,31 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int char_count = 0;
-	char c, *s;
 
-	if (format == NULL)
-		return (-1);
 	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
 	while (*format)
 	{
 		if (*format == '%')
 		{
 			format++;
-			if (*format == '\0')
-				break;
 			if (*format == 's')
 			{
-				s = va_arg(args, char*);
-				print_string(s);
-				char_count += count_string(s);
+				char *str = va_arg(args, char*);
+				int len = 0;
+
+				while (str[len] != '\0')
+					len++;
+				write(1, str, len);
+				char_count += len;
 			}
 			else if (*format == 'c')
 			{
-				c = va_arg(args, int);
+				char c = va_arg(args, int);
+
 				write(1, &c, 1);
 				char_count++;
 			}
